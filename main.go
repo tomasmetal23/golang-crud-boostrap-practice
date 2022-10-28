1238106a099ea21de87f77c570606cba043eb75c
natebrako <tomasmetal23@gmail.com>
2022-10-28 14:11:28 -0400

    funcion para editar y actualizar la informacion de registro de los empleados

--- main.go
+++ main.go
@@ -28,66 +28,19 @@ func conexionBD() (conexion *sql.DB) {
 var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main() {
+	//Rutas
 	http.HandleFunc("/", Inicio)
 	http.HandleFunc("/crear", Crear)
 	http.HandleFunc("/insertar", Insertar)
 	http.HandleFunc("/borrar", Borrar)
 	http.HandleFunc("/editar", Editar)
+	http.HandleFunc("/actualizar", Actualizar)
 	//Para integrar un nuevo directorio local en el proyecto
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Println("Running Service...")
 
 	http.ListenAndServe(":8080", nil)
-}
-
-// Funccion editar
-func Editar(w http.ResponseWriter, r *http.Request) {
-	idEmpleado := r.URL.Query().Get("id")
-	fmt.Println(idEmpleado)
-	conexionEstablecida := conexionBD()
-
-	registros, err := conexionEstablecida.Query("SELECT * FROM empleados WHERE id=?", idEmpleado)
-	empleado := Empleado{}
-	for registros.Next() {
-		var id int
-		var nombre, correo string
-		err = registros.Scan(&id, &nombre, &correo)
-		if err != nil {
-			panic(err.Error())
-		}
-		empleado.Id = id
-		empleado.Nombre = nombre
-		empleado.Correo = correo
-	}
-
-	fmt.Println(empleado)
-	plantillas.ExecuteTemplate(w, "editar", empleado)
-
-}
-
-// Funcion borrar
-func Borrar(w http.ResponseWriter, r *http.Request) {
-	idEmpleado := r.URL.Query().Get("id")
-	fmt.Println(idEmpleado)
-
-	conexionEstablecida := conexionBD()
-	//Instruccion SQL para borrar
-	borrarRegistros, err := conexionEstablecida.Prepare("DELETE FROM empleados WHERE id=?")
-
-	if err != nil {
-		panic(err.Error())
-	}
-	borrarRegistros.Exec(idEmpleado)
-
-	http.Redirect(w, r, "/", 301)
-
-}
-
-type Empleado struct {
-	Id     int
-	Nombre string
-	Correo string
 }
 
 func Inicio(w http.ResponseWriter, r *http.Request) {
@@ -147,5 +100,74 @@ func Insertar(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", 301)
 
 	}
+}
 
+// funccion que actualiza los datos desde un form con el metodo "POST" desde un formulario a la BD
+func Actualizar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nombre := r.FormValue("nombre")
+		correo := r.FormValue("correo")
+
+		conexionEstablecida := conexionBD()
+
+		modificarRegistros, err := conexionEstablecida.Prepare(" UPDATE empleados SET nombre=?,correo=? WHERE id=? ")
+
+		if err != nil {
+			panic(err.Error())
+		}
+		modificarRegistros.Exec(nombre, correo, id)
+
+		http.Redirect(w, r, "/", 301)
+
+	}
+}
+
+// Funccion editar
+func Editar(w http.ResponseWriter, r *http.Request) {
+	idEmpleado := r.URL.Query().Get("id")
+	fmt.Println(idEmpleado)
+	conexionEstablecida := conexionBD()
+
+	registros, err := conexionEstablecida.Query("SELECT * FROM empleados WHERE id=?", idEmpleado)
+	empleado := Empleado{}
+	for registros.Next() {
+		var id int
+		var nombre, correo string
+		err = registros.Scan(&id, &nombre, &correo)
+		if err != nil {
+			panic(err.Error())
+		}
+		empleado.Id = id
+		empleado.Nombre = nombre
+		empleado.Correo = correo
+	}
+
+	fmt.Println(empleado)
+	plantillas.ExecuteTemplate(w, "editar", empleado)
+
+}
+
+// Funcion borrar
+func Borrar(w http.ResponseWriter, r *http.Request) {
+	idEmpleado := r.URL.Query().Get("id")
+	fmt.Println(idEmpleado)
+
+	conexionEstablecida := conexionBD()
+	//Instruccion SQL para borrar
+	borrarRegistros, err := conexionEstablecida.Prepare("DELETE FROM empleados WHERE id=?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	borrarRegistros.Exec(idEmpleado)
+
+	http.Redirect(w, r, "/", 301)
+
+}
+
+type Empleado struct {
+	Id     int
+	Nombre string
+	Correo string
 }
